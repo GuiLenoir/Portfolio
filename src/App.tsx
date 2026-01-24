1fbb60c37905d342070040404125a51afc224050
GuiLenoir <guilhermelenoir@example.com>
2026-01-24 16:16:35 -0300

    Ajusta basename do Router para GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Portfolio/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/repositorio/:repoName" element={<Repositorio />} />
